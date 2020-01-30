56416c671aab03ccc2f9f60d6190917fc120f0b0
marend <marcus.enderskog@nepartner.se>
2020-01-30 12:51:10 +0100

    selectable port

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/static', express.static('static'))
+app.use('/static', express.static(__dirname + '/static'))
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
 
-var server = app.listen(5000, function () {
-    console.log('Node server is running..');
+var server = app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
