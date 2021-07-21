6604ce99a36f8c78339a11c031060c67fd01b1e7
marend <marcus.enderskog@nepartner.se>
2021-07-21 15:50:27 +0200

    Showing/not showing footer when navbar is collapsed/not collapsed

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,7 @@
 var images = [
   {src: "static/images/1.jpg"},
   {src: "static/images/3.jpg"},
-  /*{src: "static/images/4.jpg", align: 'right'},
-  {src: "static/images/5.jpg", align: 'left'},
-  {src: "static/images/6.jpg"},
-  {src: "static/images/7.jpg"},
-  {src: "static/images/8.jpg", align: 'left'},
-  {src: "static/images/9.jpg"},
-  {src: "static/images/10.jpg"},*/
   {src: "static/images/11.jpg"},
-  /*{src: "static/images/12.jpg"},*/
   {src: "static/images/13.jpg", align: 'right', valign: 'top'},
   {src: "static/images/2.jpg"},
 ];
